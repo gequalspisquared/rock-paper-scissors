1429b31082f79d9dd563c0a35fa754a06da458da
Nicholas - Windows <nickcrusher30@gmail.com>
2022-09-13 20:46:20 -0400

    Finish game

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -3,6 +3,7 @@ console.log("Hello, World!");
 const tie = "Tie!";
 const playerWin = "Player wins!";
 const computerWin = "Computer wins!";
+const invalidSelection = "invalid";
 
 function getComputerChoice() {
     let guess = Math.floor(Math.random() * 3);
@@ -26,10 +27,35 @@ function playRound(playerSelection, computerSelection) {
         (computerSelection === "scissors" && playerSelection === "paper")) {
             return computerWin;
         }
+    
+   return invalidSelection; 
 }
 
 function game(numGames) {
+    let score = 0;
+
     for (let i = 0; i < numGames; i++) {
-        
+        let playerSelection = prompt("What is your move?").toLowerCase();
+        let computerSelection = getComputerChoice();
+        let roundResult = playRound(playerSelection, computerSelection);
+        if (roundResult === invalidSelection) {
+            console.log("Please enter 'rock', 'paper', or 'scissors'");
+            i--;
+            continue;
+        }
+
+        console.log(roundResult);
+        if (roundResult === playerWin) {
+            score++;
+        } 
+        if (score > Math.floor(numGames / 2)) {
+            console.log("You won the game!");
+            return;
+        }
     }
+
+    console.log("The computer won the game!");
+    return;
 }
+
+game(5);
