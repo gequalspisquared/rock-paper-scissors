c13c0386d6f8a53aa6f9c7b8cb4f87a9e5a36d0d
Nicholas Crane <nickcrusher30@gmail.com>
2022-09-27 21:58:19 -0400

    complete main rps.js file, need to add gui to display score and winner

---
+++ rps.js
@@ -0,0 +1,58 @@
+const pointsToWin = 5;
+
+const tie = "Tie!";
+const playerWin = "Player wins!";
+const computerWin = "Computer wins!";
+const invalidSelection = "invalid";
+
+let playerScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+    let guess = Math.floor(Math.random() * 3);
+
+    if (guess === 0) { return "rock"; }
+    if (guess === 1) { return "paper"; }
+    if (guess === 2) { return "scissors"; }
+}
+
+function getRoundWinner(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) { return tie; }
+
+    if ((playerSelection === "rock" && computerSelection === "scissors") || 
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")) {
+            playerScore++;
+            return playerWin;
+        }
+
+    if ((computerSelection === "rock" && playerSelection === "scissors") || 
+        (computerSelection === "paper" && playerSelection === "rock") ||
+        (computerSelection === "scissors" && playerSelection === "paper")) {
+            computerScore++;
+            return computerWin;
+        }
+    
+   return invalidSelection; 
+}
+
+function checkForWin() {
+    let win = false;
+    if (playerScore === pointsToWin) { win = true; alert(playerWin); }
+    if (computerScore === pointsToWin) { win = true; alert(computerWin); } 
+
+    if (win) { playerScore = 0; computerScore = 0; win = false; }
+}
+
+function playRound (playerSelection) {
+    const computerSelection = getComputerChoice();
+    getRoundWinner(playerSelection, computerSelection);
+    checkForWin();
+    console.log("playerScore: " + playerScore + " computerScore: " + computerScore);
+}
+
+const options = document.querySelectorAll('.option');
+options.forEach(option => option.addEventListener('click', function() {
+    const playerSelection = option.getAttribute("data-option");
+    playRound(playerSelection);
+}));
