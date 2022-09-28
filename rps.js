06d937f730199001887484d75c44f62bb7659f79
Nicholas Crane <nickcrusher30@gmail.com>
2022-09-28 06:03:25 -0400

    Add gui

--- rps.js
+++ rps.js
@@ -4,6 +4,7 @@ const tie = "Tie!";
 const playerWin = "Player wins!";
 const computerWin = "Computer wins!";
 const invalidSelection = "invalid";
+const scoreboard = document.querySelectorAll('.scoreboard > ul > li');
 
 let playerScore = 0;
 let computerScore = 0;
@@ -44,13 +45,32 @@ function checkForWin() {
     if (win) { playerScore = 0; computerScore = 0; win = false; }
 }
 
-function playRound (playerSelection) {
-    const computerSelection = getComputerChoice();
-    getRoundWinner(playerSelection, computerSelection);
-    checkForWin();
-    console.log("playerScore: " + playerScore + " computerScore: " + computerScore);
+function updateScoreboard(winner, playerSelection, computerSelection) {
+    let scoreboardPlayer = scoreboard[0];
+    let scoreboardWinner = scoreboard[1];
+    let scoreboardComputer = scoreboard[2];
+
+    if (playerSelection) {
+        scoreboardPlayer.textContent = playerSelection + ' : ' + 
+                                       playerScore;
+    }
+    scoreboardWinner.textContent = winner;
+    if (computerSelection) {
+        scoreboardComputer.textContent = computerScore + ' : ' + 
+                                         computerSelection;
+    }
 }
 
+function playRound (playerSelection) {
+    const computerSelection = getComputerChoice();
+    const winner = getRoundWinner(playerSelection, computerSelection);
+    checkForWin();
+    updateScoreboard(winner, playerSelection, computerSelection);
+    console.log("playerScore: " + playerScore + 
+                " computerScore: " + computerScore);
+}
+
+updateScoreboard("Welcome!");
 const options = document.querySelectorAll('.option');
 options.forEach(option => option.addEventListener('click', function() {
     const playerSelection = option.getAttribute("data-option");
